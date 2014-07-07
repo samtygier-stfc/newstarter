742c5a1c04a282b0663b5c7d4ce9782a7ebc04f4
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2014-07-07 09:16:30 +0100

    Add basic template for exercise 1.

---
+++ CMake/CommonSetup.cmake
@@ -0,0 +1,13 @@
+###############################################################################
+# CommonSetup.cmake
+###############################################################################
+
+# We want shared libraries everywhere
+set ( BUILD_SHARED_LIBS On )
+
+# Send libraries to common place
+set ( CMAKE_RUNTIME_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/bin )
+set ( CMAKE_LIBRARY_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/bin )
+
+# This allows us to group targets logically in Visual Studio
+set_property ( GLOBAL PROPERTY USE_FOLDERS ON )
