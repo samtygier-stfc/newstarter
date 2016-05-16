ce1615a9af76ac5fb98ef26bfaa73cfd066c55e3
Owen Arnold <owen.arnold@stfc.ac.uk>
2016-05-16 16:39:43 +0100

    c++11 compiler flag

--- CMake/CommonSetup.cmake
+++ CMake/CommonSetup.cmake
@@ -12,8 +12,4 @@ set ( CMAKE_LIBRARY_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/bin )
 # This allows us to group targets logically in Visual Studio
 set_property ( GLOBAL PROPERTY USE_FOLDERS ON )
 
-
-if ( CMAKE_COMPILER_IS_GNUCXX )
-  # Use c++0x flag for gcc (same as mantid for the time being)
-  set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x" )
-endif ()
+set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11" )
