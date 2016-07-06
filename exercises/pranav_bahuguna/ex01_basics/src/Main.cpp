f56b8f561613e18fdf81b562efbca26dd8f1ca30
PranavBahuguna <pranav.bahuguna@gmail.com>
2016-07-06 09:38:39 +0100

    ex01 can read user input and produce list of unique words

--- exercises/pranav_bahuguna/ex01_basics/src/Main.cpp
+++ exercises/pranav_bahuguna/ex01_basics/src/Main.cpp
@@ -1,4 +1,44 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
 int main()
 {
+	int i = 0;
+	string s;
+	map<string, int> counters;
+
+	// obtain user input
+	cout << "Type a sentence: ";
+	getline(cin, s);
+
+	while (i != s.size())
+	{
+		// skip over leading blanks or hyphens
+		while (isspace(s.at(i)) || s[i] == '-')
+			i++;
+
+		// find end of next word
+		int j = i;
+		while (j != s.size() && !isspace(s[j]) && s[j] != '-')
+			j++;
+		
+		// if non-whitespace characters found, it is a word
+		if (i != j) {
+			// only words longer than 4 characters are included
+			if (j - i > 4)
+				counters[s.substr(i, j - i)]++;
+			i = j;
+		}
+	}
+
+	// write words and associated counts
+	for (map<string, int>::const_iterator it = counters.begin();
+		it != counters.end(); it++) {
+		cout << it->first << "\t" << it->second << endl;
+	}
+
 	return 0;
 }
