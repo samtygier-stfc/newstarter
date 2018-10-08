9985320390d388616a8c05f52c028897dbe49210
Akello-Egwel <ogf9148403@fed.cclrc.ac.uk>
2018-10-08 10:32:46 +0100

    Opening and reading a file

---
+++ exercises/dolica_akelloegwel/ex01_basics/src/main.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+/**
+ * Skeleton main routine
+ */
+
+void ReadFile(std::string ascii_filename)
+{
+	std::ifstream inFile;
+	std::string line;
+
+	inFile.open(ascii_filename);
+
+	if (!inFile)
+	{
+		std::cout << "Error: Unable to load the file " << ascii_filename << "." << std::endl;
+		exit(1);
+	}
+
+	while (!inFile.eof())
+	{
+		inFile >> line;
+		std::cout << line;
+	}
+
+	inFile.close();
+
+}
+int main(int argc, char *argv[])
+{
+	std::string ascii_filename;
+
+	if (argc == 2)
+	{
+		ascii_filename = argv[1];
+	}
+	else
+	{
+		std::cout << "Error: Incorrect number of arguments given. Expected 2." << std::endl;
+		exit(1);
+	}
+
+	ReadFile(ascii_filename);
+
+}
