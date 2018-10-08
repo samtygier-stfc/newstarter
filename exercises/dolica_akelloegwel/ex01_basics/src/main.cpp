dcc37b6a4e1d2f04e048de148214b5921320671d
Akello-Egwel <ogf9148403@FED.CCLRC.AC.UK>
2018-10-08 11:19:00 +0100

    Adding comments

--- exercises/dolica_akelloegwel/ex01_basics/src/main.cpp
+++ exercises/dolica_akelloegwel/ex01_basics/src/main.cpp
@@ -8,42 +8,58 @@
 
 void ReadFile(std::string asciiFilename)
 {
+	// Declare a hash table for storing the word counts
 	std::map<std::string, int> wordMap;
+
+	// Declare a stream for the text file
 	std::ifstream inFile;
+
+	// Declare a string for the words in the text 
 	std::string word;
 	
+	// Declare a string of punctuation characters
 	std::string unwantedChars = ".,?'\"!():";
 
+	// Attempt to open the file
 	inFile.open(asciiFilename);
 
+	// Check that the file was opened successfully
 	if (!inFile)
 	{
 		std::cout << "Error: Unable to load the file " << asciiFilename << "." << std::endl;
 		exit(1);
 	}
 
+	// Traverse through the words in the file
 	while (!inFile.eof())
 	{
+		// Store the word in a string
 		inFile >> word;
 
+		// Remove the unwanted characters from the string
 		for (int i = 0; i < unwantedChars.length(); i++)
 		{
 			word.erase(std::remove(word.begin(), word.end(), unwantedChars[i]), word.end());
 		}
 
+		// Check that the word has at least five characters
 		if (word.length() <= 4)
 			continue;
 
+		// Convert the word to lower case
 		for (unsigned int i = 0; i < word.length(); ++i) 
 		{
 			word[i] = std::tolower(word[i]);
 		}
 
+		// Place the word in the hash table
 		wordMap[word]++;
 	}
 
+	// Close the file
 	inFile.close();
 
+	// Print the contents of the wordmap
 	for (auto it = wordMap.cbegin(); it != wordMap.cend(); ++it)
 	{
 		std::cout << it->first << " - " << it->second << std::endl;
@@ -52,9 +68,10 @@ void ReadFile(std::string asciiFilename)
 }
 int main(int argc, char *argv[])
 {
-	
+	// Declare a string for the filename
 	std::string asciiFilename;
 
+	// Check that the correct number of arguments were given
 	if (argc == 2)
 	{
 		asciiFilename = argv[1];
@@ -65,6 +82,7 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
+	// Pass the filename argument to the word count function
 	ReadFile(asciiFilename);
 
 }
