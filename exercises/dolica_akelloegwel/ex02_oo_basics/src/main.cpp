9b8478a730dda01e13a46a1bed844be648fa2a7d
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2018-10-09 12:09:26 +0100

    Placing the shapes in a vector

--- exercises/dolica_akelloegwel/ex02_oo_basics/src/main.cpp
+++ exercises/dolica_akelloegwel/ex02_oo_basics/src/main.cpp
@@ -3,28 +3,42 @@
  */
 
 #include <iostream>
+#include <vector>
+
 #include "Square.h"
 #include "Rectangle.h"
 #include "Circle.h"
 #include "Triangle.h"
 
+#ifndef _Shape_h
+#define _Shape_h
+
 int main(int, char **)
 {
 	double squareSide = 2;
 	Square mySquare = Square::Square(squareSide);
-	std::cout << mySquare.GetPerimeter() << std::endl;
 
 	double recWidth = 2;
 	double recHeight = 5;
 	Rectangle myRectangle = Rectangle::Rectangle(recWidth, recHeight);
-	std::cout << myRectangle.GetType() << std::endl;
 
 	double circleRadius = 5;
 	Circle myCircle = Circle::Circle(circleRadius);
-	std::cout << myCircle.GetArea() << std::endl;
 
 	double triangleBase = 3;
 	double triangleHeight = 4;
 	Triangle myTriangle = Triangle::Triangle(triangleBase, triangleHeight);
-	std::cout << myTriangle.GetArea() << std::endl;
+
+	std::vector<Shape> myShapes;
+
+	myShapes.push_back(mySquare);
+	myShapes.push_back(myRectangle);
+	myShapes.push_back(myCircle);
+	myShapes.push_back(myTriangle);
+
+	for (std::vector<Shape>::iterator it = myShapes.begin(); it != myShapes.end(); it++) {
+		std::cout << it->GetType() << std::endl;
+	}
 }
+
+#endif
