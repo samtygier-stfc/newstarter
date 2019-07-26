2b6fcfac4aba5c2516b9a2e69964f7c77ace5f7b
Harriet Brown <harriet.brown@stfc.ac.uk>
2019-07-26 13:51:52 +0100

    Catch missmatch type for command line input.

--- exercises/harriet_brown/ex02_oo_basics/inc/getInput.h
+++ exercises/harriet_brown/ex02_oo_basics/inc/getInput.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <limits>
 
 void getInput(std::string question_text, int &ans);
 void getInput(std::string question_text, double &ans);
--- exercises/harriet_brown/ex02_oo_basics/src/getInput.cpp
+++ exercises/harriet_brown/ex02_oo_basics/src/getInput.cpp
@@ -5,7 +5,19 @@ void getInput(std::string question_text, int &ans)
 {
 	std::cout << question_text;
 	std::cin >> ans;
-	std::cout << std::endl;
+	std::cout << std::endl; 
+	while (1)
+	{
+		if (std::cin.fail())
+		{
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cout << "You have entered wrong input" << std::endl;
+			std::cin >> ans;
+		}
+		if (!std::cin.fail())
+			break;
+	}
 	return;
 }
 
@@ -14,6 +26,18 @@ void getInput(std::string question_text, double &ans)
 	std::cout << question_text;
 	std::cin >> ans;
 	std::cout << std::endl;
+	while (1)
+	{
+		if (std::cin.fail())
+		{
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cout << "You have entered wrong input" << std::endl;
+			std::cin >> ans;
+		}
+		if (!std::cin.fail())
+			break;
+	}
 	return;
 }
 
@@ -22,5 +46,17 @@ void getInput(std::string question_text, std::string &ans)
 	std::cout << question_text;
 	std::cin >> ans;
 	std::cout << std::endl;
+	while (1)
+	{
+		if (std::cin.fail())
+		{
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cout << "You have entered wrong input" << std::endl;
+			std::cin >> ans;
+		}
+		if (!std::cin.fail())
+			break;
+	}
 	return;
 }
