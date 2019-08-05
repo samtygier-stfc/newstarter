e1e7701f57bc8ae7fe5a5566ced8041a838538ed
Harriet Brown <harriet.brown@stfc.ac.uk>
2019-08-05 10:39:15 +0100

    Apply changes from code review

--- exercises/harriet_brown/ex01_basics/src/main.cpp
+++ exercises/harriet_brown/ex01_basics/src/main.cpp
@@ -12,12 +12,17 @@
 
 void remove_punctuation_in_string(std::string &data)
 {
-	std::vector<std::string> puntuation{ ".", ",", "?", "!", "\'", "\"", "(", ")", ":", "--", ";"};
+	// Remove all cases of punctuation in a line
+	std::string puntuation{ ".,?!\'\"():;"};
 	for (auto x : puntuation) {
 		while (data.find(x) != std::string::npos) {
 			data.replace(data.find(x), 1, " ");
 		}
 	}
+	while (data.find("--") != std::string::npos) {
+		data.replace(data.find("--"), 2, " ");
+	}
+	// Convert all letters in lower case
 	std::transform(data.begin(), data.end(), data.begin(), ::tolower);
 	return;
 }
@@ -26,23 +31,28 @@ void print_map_by_value(std::map<std::string, int> map_to_print)
 {
 	std::ofstream result_file;
 	result_file.open("word_count.txt");
-	std::cout << std::setw(18) << "Word" << std::setw(12) << "Count" << std::endl;
+	if (!result_file) {
+		std::cerr << "Unable to open results file word_count.txt";
+		exit(1);   // call system to stop
+	}
 	result_file << std::setw(18) << "Word" << std::setw(12) << "Count" << std::endl;
-	std::string max_key;
-	int max_val;
 	while (map_to_print.size() > 0) {
-		max_val = 0;
-		for (auto it = map_to_print.begin(); it != map_to_print.end(); ++it) {
-			if (it->second > max_val) {
-				max_val = it->second;
-				max_key = it->first;
+		// loop over word map
+		std::string max_key;
+		int max_val = 0;
+		for (const auto& it : map_to_print) {
+			if (it.second > max_val) {
+				// find largest word count
+				max_val = it.second;
+				max_key = it.first;
 			}
 		}
-		std::cout << std::setw(18) << max_key << std::setw(12) << max_val << std::endl;
+		// save word and count to file
 		result_file << std::setw(18) << max_key << std::setw(12) << max_val << std::endl;
+		// remove largest count word from map
 		map_to_print.erase(max_key);
-		max_val = 0;
 	}
+	result_file.close();
 	return;
 }
 
@@ -57,7 +67,11 @@ int main(int argc, char* argv[])
 	// Check the number of parameters
 	if (argc < 2) {
 		std::cerr << argv[0] << " needs to be given a file directory as an argument" << std::endl;
-		return 0;
+		exit(1);   // call system to stop
+	}
+	if (argc > 2) {
+		std::cerr << "Too many arguments, " << argv[0] << " needs to be given a file directory as an argument" << std::endl;
+		exit(1);   // call system to stop
 	}
 	ascii_file.open(argv[1]);
 	if (!ascii_file) {
@@ -72,15 +86,9 @@ int main(int argc, char* argv[])
 	while (str_strm >> tmp) {
 		words.push_back(tmp);
 	}
-	
-	for (unsigned int i = 0; i < words.size(); i++) {
-		if (words[i].size() > 4) {
-			if (word_count_map.find(words[i]) != word_count_map.end()) {
-				word_count_map[words[i]]++;
-			}
-			else {
-				word_count_map[words[i]] = 1;
-			}
+	for (const auto& word : words) {
+		if (word.size() > 4) {
+			word_count_map[word]++;
 		}
 	}
 	print_map_by_value(word_count_map);
