21b4bd3f9533b64183bd072b83984838dc86de3b
Giovanni Di Siena <giodisiena@gmail.com>
2019-08-07 14:39:31 +0100

    Updated typedefs, declarations and formatting

--- exercises/giovanni_disiena/ex01_basics/src/main.cpp
+++ exercises/giovanni_disiena/ex01_basics/src/main.cpp
@@ -1,42 +1,44 @@
 #include "main.h"
 
 int main(int argc, char **argv) {
-	// prompt user for filename
-	std::cout << "Please enter the name of a file you wish to read: ";
-	std::string file_name;
-	std::cin >> file_name;
+  // prompt user for filename
+  std::cout << "Please enter the name of a file you wish to read: ";
+  std::string file_name;
+  std::cin >> file_name;
 
-	// create map
-	MapType wordCounts;
+  // create map
+  StrIntMap wordCounts;
 
-	//declare file read boolean and unsigned max length
-	bool read_bool;
-	std::size_t max_len = 0;
+  // declare file read boolean and unsigned max length
+  bool read_bool;
+  std::size_t max_len = 0;
 
-	// read file
-	try {
-		read_bool = readFile(file_name, wordCounts, max_len);
-	} catch (const char* msg) {
-		std::cerr << msg << std::endl;
-		read_bool = false;
-	}
+  // read file
+  try {
+    read_bool = readFile(file_name, wordCounts, max_len);
+  } catch (const char *msg) {
+    std::cerr << msg << std::endl;
+    read_bool = false;
+  }
 
-	//define lambda for comparison logic
-	Comparator compFunctor =
-		[](const Pair& elem1, const Pair& elem2) { return elem1.second > elem2.second; };
+  // define lambda for comparison logic
+  Comparator compFunctor = [](const StrIntPair &elem1,
+                              const StrIntPair &elem2) {
+    return elem1.second > elem2.second;
+  };
 
-	// declare vector
-	std::vector<Pair> ordered_vector;
+  // declare vector
+  std::vector<StrIntPair> ordered_vector;
 
-	// order words
-	orderWords(wordCounts, ordered_vector, compFunctor);
+  // order words
+  orderWords(wordCounts, ordered_vector, compFunctor);
 
-	// write file
-	try {
-		writeFile(ordered_vector, max_len, read_bool);
-	} catch (const char* msg) {
-		std::cerr << msg << std::endl;
-	}
+  // write file
+  try {
+    writeFile(ordered_vector, max_len, read_bool);
+  } catch (const char *msg) {
+    std::cerr << msg << std::endl;
+  }
 }
 
 /** Alters string to lower case
@@ -45,11 +47,10 @@ int main(int argc, char **argv) {
  * @return word: string with all capitals lowered
  *
  */
-
-std::string str_tolower(std::string& word) {
-	std::transform(word.begin(), word.end(), word.begin(),
-		[](unsigned char c) { return std::tolower(c); });
-	return word;
+std::string str_tolower(std::string &word) {
+  std::transform(word.begin(), word.end(), word.begin(),
+                 [](unsigned char c) { return std::tolower(c); });
+  return word;
 }
 
 /** Takes a string from which to clean specified characters from
@@ -60,11 +61,12 @@ std::string str_tolower(std::string& word) {
  * @return void
  *
  */
-
-void cleanWord(std::string& word, std::vector<std::string>& cleaned_words) {
-	static const std::regex string_regex("(\\B'\b|\b'\\B)|([\\.,:;!?()\"-]+)");
-	auto first = std::sregex_token_iterator(word.begin(), word.end(), string_regex, -1);
-	std::copy(first, std::sregex_token_iterator(), std::back_inserter(cleaned_words));
+void cleanWord(std::string &word, std::vector<std::string> &cleaned_words) {
+  static const std::regex string_regex("(\\B'\b|\b'\\B)|([\\.,:;!?()\"-]+)");
+  auto first =
+      std::sregex_token_iterator(word.begin(), word.end(), string_regex, -1);
+  std::copy(first, std::sregex_token_iterator(),
+            std::back_inserter(cleaned_words));
 }
 
 /** Loads a .txt file, checking for success. Subsequently reads the file
@@ -76,33 +78,32 @@ void cleanWord(std::string& word, std::vector<std::string>& cleaned_words) {
  * @return bool: check for read success
  *
  */
-
-bool readFile(const std::string& filename, MapType& inputmap, std::size_t& max_len, const std::size_t char_min) {
-	// attempt to open file
-	std::ifstream inFile;
-	inFile.open(filename + ".txt");
-	if (!inFile) {
-		throw "Unable to open requested file";
-	}
-	else {
-		// read contents to map
-		std::string word;
-		while (inFile >> word) {
-			// clean words
-			std::vector<std::string> cleaned_words;
-			std::string lowered = str_tolower(word);
-			cleanWord(lowered, cleaned_words);
-			for (auto it = cleaned_words.begin(); it != cleaned_words.end(); ++it) {
-				if ((*it).size() > char_min) {
-					++inputmap[*it];
-					if ((*it).size() > max_len) {
-						max_len = (*it).size();
-					}
-				}
-			}
-		}
-		return true;
-	}
+bool readFile(const std::string &filename, StrIntMap &inputmap,
+              std::size_t &max_len, const std::size_t char_min) {
+  // attempt to open file
+  std::ifstream inFile;
+  inFile.open(filename + ".txt");
+  if (!inFile) {
+    throw "Unable to open requested file";
+  } else {
+    // read contents to map
+    std::string word;
+    while (inFile >> word) {
+      // clean words
+      std::vector<std::string> cleaned_words;
+      std::string lowered = str_tolower(word);
+      cleanWord(lowered, cleaned_words);
+      for (auto it = cleaned_words.begin(); it != cleaned_words.end(); ++it) {
+        if ((*it).size() > char_min) {
+          ++inputmap[*it];
+          if ((*it).size() > max_len) {
+            max_len = (*it).size();
+          }
+        }
+      }
+    }
+    return true;
+  }
 }
 
 /** Orders unique words in non-increasing order
@@ -113,11 +114,12 @@ bool readFile(const std::string& filename, MapType& inputmap, std::size_t& max_l
  * @return void
  *
  */
-
-void orderWords(const MapType& cleanedMap, std::vector<Pair>& outputVector, const Comparator& cmp) {
-	outputVector.reserve(cleanedMap.size());
-	std::copy(cleanedMap.begin(), cleanedMap.end(), std::back_inserter(outputVector));
-	std::sort(outputVector.begin(), outputVector.end(), cmp);
+void orderWords(const StrIntMap &cleanedMap,
+                std::vector<StrIntPair> &outputVector, const Comparator &cmp) {
+  outputVector.reserve(cleanedMap.size());
+  std::copy(cleanedMap.begin(), cleanedMap.end(),
+            std::back_inserter(outputVector));
+  std::sort(outputVector.begin(), outputVector.end(), cmp);
 }
 
 /** Determines whitespace formatting of output file
@@ -127,10 +129,10 @@ void orderWords(const MapType& cleanedMap, std::vector<Pair>& outputVector, cons
  * @return ws: string of appropriate number of white spaces
  *
  */
-
-const std::string formatWhitespace(const std::string& str_1, const std::size_t& max_len) {
-	std::string ws(max_len - str_1.length(), ' ');
-	return ws;
+const std::string formatWhitespace(const std::string &str_1,
+                                   const std::size_t &max_len) {
+  std::string ws(max_len - str_1.length(), ' ');
+  return ws;
 }
 
 /** Writes the results extracted from the map, into vector of pairs, to file
@@ -141,20 +143,18 @@ const std::string formatWhitespace(const std::string& str_1, const std::size_t& 
  * @return void
  *
  */
-
-const void writeFile(const std::vector<Pair>& orderedPair, const std::size_t& max_len, bool& read_bool) {
-	if (!read_bool) {
-		throw "Unable to write file";
-	}
-	else {
-		std::ofstream outfile("word_counts.txt");
-		std::string header_1 = "Word", header_2 = "Usage";
-		outfile << header_1 << formatWhitespace(header_1, max_len) << header_2 << "\n\n";
-		for (auto it = orderedPair.begin();
-			it != orderedPair.end(); ++it) {
-			outfile << it->first << formatWhitespace(it->first, max_len) << it->second << '\n';
-		}
-	}
+const void writeFile(const std::vector<StrIntPair> &orderedPair,
+                     const std::size_t &max_len, bool &readBool) {
+  if (!readBool) {
+    throw "Unable to write file";
+  } else {
+    std::ofstream outfile("word_counts.txt");
+    std::string header_1 = "Word", header_2 = "Usage";
+    outfile << header_1 << formatWhitespace(header_1, max_len) << header_2
+            << "\n\n";
+    for (auto it = orderedPair.begin(); it != orderedPair.end(); ++it) {
+      outfile << it->first << formatWhitespace(it->first, max_len) << it->second
+              << '\n';
+    }
+  }
 }
-
-// implementation is slow :(
--- exercises/giovanni_disiena/ex01_basics/src/main.h
+++ exercises/giovanni_disiena/ex01_basics/src/main.h
@@ -11,15 +11,19 @@
 
 // declare map/lamba/pair/set types which will store values
 // in non-increasing order unsing comparison logic
-typedef std::map<std::string, int> MapType;
-typedef std::pair<std::string, int> Pair;
-typedef const std::function<bool(Pair, Pair)>& Comparator;
+typedef std::map<std::string, int> StrIntMap;
+typedef std::pair<std::string, int> StrIntPair;
+typedef const std::function<bool(StrIntPair, StrIntPair)> &Comparator;
 
-std::string str_tolower(std::string&);
-void cleanWord(std::string&, std::vector<std::string>&);
-void orderWords(const MapType&, std::vector<Pair>&, const Comparator&);
-const std::string formatWhitespace(const std::string&, const std::size_t&);
-bool readFile(const std::string&, MapType&, std::size_t&, const std::size_t = 4);
-const void writeFile(const std::vector<Pair>&, const std::size_t&, bool&);
+std::string str_tolower(std::string &word);
+void cleanWord(std::string &word, std::vector<std::string> &cleaned_words);
+void orderWords(const StrIntMap &cleanedMap,
+								std::vector<StrIntPair> &outputVector, const Comparator &cmp);
+const std::string formatWhitespace(const std::string &str_1,
+																	 const std::size_t &max_len);
+bool readFile(const std::string &filename, StrIntMap &inputmap,
+							std::size_t &max_len, const std::size_t char_min = 4);
+const void writeFile(const std::vector<StrIntPair> &orderedPair,
+										 const std::size_t &, bool &readBool);
 
 #endif // MAIN_H
