8ffc7d2ab1c232eac64a2fb7d5aacc5daaa5eb46
Mathieu Tillet <tillet@ill.eu>
2019-12-09 15:46:24 +0100

    Fixed print_by_area and print_by_perimeter. Now print in decreasing order, rather than increasing

--- exercises/Mathieu_Tillet/ex02_oo_basics/src/shapesorter.cpp
+++ exercises/Mathieu_Tillet/ex02_oo_basics/src/shapesorter.cpp
@@ -3,7 +3,6 @@
 ShapeSorter::ShapeSorter(){}
 
 void ShapeSorter::print_by_sides(vector<Shape*> &vec, int side_nb, ostream &os){
-
     for (vector<Shape*>::iterator it = vec.begin(); it != vec.end(); ++it){
         if ((*it)->sides == side_nb){
             (*it)->display(os);
@@ -12,7 +11,6 @@ void ShapeSorter::print_by_sides(vector<Shape*> &vec, int side_nb, ostream &os){
 }
 
 void ShapeSorter::print_by_type(vector<Shape*> &vec, string type, ostream &os){
-
     for (vector<Shape*>::iterator it = vec.begin(); it != vec.end(); ++it){
         if ((*it)->shape_type == type){
             (*it)->display(os);
@@ -21,11 +19,11 @@ void ShapeSorter::print_by_type(vector<Shape*> &vec, string type, ostream &os){
 }
 
 bool perimeter_comp(Shape *s1, Shape *s2){
-    return s1->perimeter() < s2->perimeter();
+    return s1->perimeter() > s2->perimeter();
 }
 
 bool area_comp(Shape *s1, Shape *s2){
-    return s1->area() < s2->area();
+    return s1->area() > s2->area();
 }
 
 void ShapeSorter::print_by_perimeter(vector<Shape*> &vec, ostream &os){
@@ -33,7 +31,6 @@ void ShapeSorter::print_by_perimeter(vector<Shape*> &vec, ostream &os){
     for (vector<Shape*>::iterator it = vec.begin(); it != vec.end(); ++it){
         (*it)->display(os);
     }
-
 }
 
 void ShapeSorter::print_by_area(vector<Shape*> &vec, ostream &os){
@@ -41,5 +38,4 @@ void ShapeSorter::print_by_area(vector<Shape*> &vec, ostream &os){
     for (vector<Shape*>::iterator it = vec.begin(); it != vec.end(); ++it){
         (*it)->display(os);
     }
-
 }
--- exercises/Mathieu_Tillet/ex02_oo_basics/src/shapesorter.h
+++ exercises/Mathieu_Tillet/ex02_oo_basics/src/shapesorter.h
@@ -1,3 +1,6 @@
+/*
+ *
+ */
 #ifndef SHAPESORTER_H
 #define SHAPESORTER_H
 
@@ -14,9 +17,16 @@ public:
     ShapeSorter();
 
     static void print_by_sides(vector<Shape*> &vec, int side_nb, ostream &os);
+    /* Print shapes corresponding to given number of sides on given output stream */
+
     static void print_by_type(vector<Shape*> &vec, string type, ostream &os);
+    /* Print shapes corresponding to given typename on given output stream */
+
     static void print_by_area(vector<Shape*> &vec, ostream &os);
+    /* Print shapes by descending area on given output stream */
+
     static void print_by_perimeter(vector<Shape*> &vec, ostream &os);
+    /* Print shapes by descending perimeter on given output stream */
 
 };
 
