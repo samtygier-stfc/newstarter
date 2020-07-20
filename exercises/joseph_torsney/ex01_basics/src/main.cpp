0397fc9d38745c969bd30d9060c6892b14fbef56
joetorsney <joetorsney@gmail.com>
2020-07-20 13:09:13 +0100

    can remove punctuation

--- exercises/joseph_torsney/ex01_basics/src/main.cpp
+++ exercises/joseph_torsney/ex01_basics/src/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream> 
 #include <fstream>
+#include <vector>
 
-std::string readFile(const char* filename)
+/*
+* readFile takes a filename of a txt file and returns its contents as a string.
+*/
+std::string readFile(const std::string filename)
 {
   std::ifstream ifs(filename);
   std::string text(
@@ -11,7 +15,29 @@ std::string readFile(const char* filename)
   return(text);
 }
 
+/*
+* removePunct takes a string and returns that string with no punctuation.
+*/
+std::string removePunct(std::string str)
+{
+  for (int i = 0, len = str.size(); i < len; i++) {
+    if (ispunct(str[i])) {
+      // if this char is punctuation, reduce i by 1, remove it, and
+      // reevaluate the string size.
+      str.erase(i--, 1);
+      len = str.size();
+    }
+  }
+  return(str);
+}
+
+std::vector<std::string> split(const std::string& s)
+{
+
+}
+
 int main(int argc, char** argv)
 {
-  std::cout << readFile(argv[1]);
+  std::string text = readFile(argv[1]);
+  std::cout << removePunct(text);
 }
