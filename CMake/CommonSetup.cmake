9045a4cbf6e85bb29e47c4202cbf35ab23efd6d5
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2020-07-20 08:04:41 +0100

    Set C++17 as minimum standard by compiler
    Matches mantid code.

--- CMake/CommonSetup.cmake
+++ CMake/CommonSetup.cmake
@@ -12,7 +12,7 @@ set ( CMAKE_LIBRARY_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/bin )
 # This allows us to group targets logically in Visual Studio
 set_property ( GLOBAL PROPERTY USE_FOLDERS ON )
 
-set ( CMAKE_CXX_STANDARD 14 )
+set ( CMAKE_CXX_STANDARD 17 )
 set ( CMAKE_CXX_STANDARD_REQUIRED ON )
 
 add_definitions ( -D_USE_MATH_DEFINES )
