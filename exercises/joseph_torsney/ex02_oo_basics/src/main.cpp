a003dfc6e7bbeb48272f4949105e06d0652eaa44
joseph-torsney <joseph.torsney@stfc.ac.uk>
2020-07-22 15:34:50 +0100

    printAreas and printPerimeters

--- exercises/joseph_torsney/ex02_oo_basics/src/main.cpp
+++ exercises/joseph_torsney/ex02_oo_basics/src/main.cpp
@@ -9,14 +9,20 @@
 
 int main(int, char **)
 {
-  Rect* myRect = new Rect(10, 20); // why do these have to be pointers?
+  Rect* myRect = new Rect(10, 200); // why do these have to be pointers?
   Square* mySquare = new Square(10);
   Circle* myCircle = new Circle(10);
-  Triangle* myTriangle = new Triangle(10, 20);
+  Triangle* myTriangle = new Triangle(10, 200);
 
   ShapeSorter* sorter = new ShapeSorter(
     { myRect, mySquare, myCircle, myTriangle }
   );
-  sorter->printMatching("triangle");
+
+  sorter->printMatching("rect");
+  std::cout << std::endl;
   sorter->printSides(4);
+  std::cout << std::endl;
+  sorter->printAreas();
+  std::cout << std::endl;
+  sorter->printPerimeters();
 }
--- exercises/joseph_torsney/ex02_oo_basics/src/shapeSorter.h
+++ exercises/joseph_torsney/ex02_oo_basics/src/shapeSorter.h
@@ -4,6 +4,7 @@
 #include "shape.h"
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 class ShapeSorter {
   public:
@@ -11,46 +12,65 @@ class ShapeSorter {
       shapes = v;
     }
   
-  /** Prints the shape objects that match a given type
-   * 
-   * @param type the type of shapes to print
-   */
-  void printMatching(std::string type) {
-    for (auto it = shapes.begin(); it != shapes.end(); it++) {
-      if (type == (*it)->getType()) {
-        std::cout << (*it)->toString() << std::endl;
+    /** Prints the shape objects that match a given type
+     * 
+     * @param type the type of shapes to print
+     */
+    void printMatching(std::string type) {
+      for (auto it = shapes.begin(); it != shapes.end(); it++) {
+        if (type == (*it)->getType()) {
+          std::cout << (*it)->toString() << std::endl;
+        }
       }
     }
-  }
 
-  /** Prints the shape objects that have a given number of sides
-   * 
-   * @param sides the number of sides of shapes to print
-   */
-  void printSides(int sides) {
-    for (auto it = shapes.begin(); it != shapes.end(); it++) {
-      if (sides == (*it)->getSides()) {
-        std::cout << (*it)->toString() << std::endl;
+    /** Prints the shape objects that have a given number of sides
+     * 
+     * @param sides the number of sides of shapes to print
+     */
+    void printSides(int sides) {
+      for (auto it = shapes.begin(); it != shapes.end(); it++) {
+        if (sides == (*it)->getSides()) {
+          std::cout << (*it)->toString() << std::endl;
+        }
       }
     }
-  }
 
-  /** Prints the areas of the shapes in descending order
-   * 
-   */
-  void printAreas() {
+    /** Prints the shapes in descending order by areas
+     * 
+     */
+    void printAreas() {
+      std::sort(shapes.begin(), shapes.end(), 
+        [](const auto& a, const auto& b) -> bool {
+          return a->getArea() > b->getArea();
+        }
+      );
+      printAll();
+    }
 
-  }
-
-  /** Prints the perimeters of the shapes in descending order
-   * 
-   */
-  void printPerimeters() {
-
-  }
+    /** Prints the shapes in descending order of perimeters
+     * 
+     */
+    void printPerimeters() {
+      std::sort(shapes.begin(), shapes.end(), 
+        [](const auto& a, const auto& b) -> bool {
+          return a->getPerimeter() > b->getPerimeter();
+        }
+      );
+      printAll();
+    }
 
   private:
     std::vector<Shape*> shapes;
+
+    /** Prints the entire shapes vector
+     * 
+     */
+    void printAll() {
+      for (auto it = shapes.begin(); it != shapes.end(); it++) {
+        std::cout << (*it)->toString() << std::endl;
+      }
+    }
 };
 
 #endif
