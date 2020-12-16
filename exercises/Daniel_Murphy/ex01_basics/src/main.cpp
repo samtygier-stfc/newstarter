afe81b3a77025cf025de70bb6867c85481f4bb31
Daniel Murphy <daniel.murphy@stfc.ac.uk>
2020-12-16 13:16:05 +0000

    Requested Changes to Ex1
    use 2 spaces instead of tab
    remove "using namespace std"
    pass vector of words by ref
    use const ref where possible
    pad columns with std::setw
    use sstream to simplfy breaking lines into words
    removing Readfile class to leave standalone functions

--- exercises/Daniel_Murphy/ex01_basics/src/main.cpp
+++ exercises/Daniel_Murphy/ex01_basics/src/main.cpp
@@ -5,147 +5,122 @@
 #include <vector>
 #include <algorithm>
 #include <functional>
-using namespace std;
+#include <iomanip>
+#include <sstream>
 
-class ReadFile
+std::vector<std::string> AnalyseLine(std::string line)
 {
-public:
-	std::vector<string> AnalyseLine(string line);
-	std::map<string, int> UpdateMap(std::vector<string> words);
-};
+  // Remove punctuation that accompanies a space
+  std::vector<char> punctuation = {'?','.',',', '\'', '"', '(', ')', '!', ':', ';'};
+  for (char mark : punctuation)
+  {
+    line.erase(std::remove(line.begin(), line.end(),mark), line.end());
+  }
 
+  // Hyphens don't accompany spaces so replace with a space and split words
+  std::replace( line.begin(), line.end(), '-', ' ');
 
-std::vector<string> ReadFile::AnalyseLine(string line)
-{
-	std::vector<int> positions = {-1}; // holds positions of all spaces, include the start of the line
+  // force all letters to case lower
+  std::transform(line.begin(), line.end(), line.begin(), [](unsigned char c){ return std::tolower(c);});
 
-	// Remove punctuation that accompanies a space
-	std::vector<char> punctuation = {'?','.',',', '\'', '"', '(', ')', '!', ':', ';'};
-	for (char mark : punctuation)
-	{
-		line.erase(std::remove(line.begin(), line.end(),mark), line.end());
-	}
+  std::vector<std::string> words;
 
-	// Hyphens don't accompany spaces so replace with a space and split words
-	std::replace( line.begin(), line.end(), '-', ' ');
+  std::stringstream lineOfText(line);
+  std::string word;
+  while (lineOfText >> word) {
+    if (word.size() > 4)
+      words.push_back(word);
+  }
 
-	// force all letters to case lower
-	std::transform(line.begin(), line.end(), line.begin(), [](unsigned char c){ return std::tolower(c);});
-
-	// Find position of space
-	int pos = static_cast<int>(line.find(" ",0));
-	while(pos != static_cast<int>(string::npos))
-	{
-		positions.push_back(pos);
-		pos = static_cast<int>(line.find(" ",pos+1));
-	}
-	positions.push_back(static_cast<int>(line.length())); // add the end of the line
-
-	// Cut the line by the space positions into words
-	std::vector<string> words;
-	for (auto iter = positions.begin(); iter != positions.end()-1; iter++)
-	{
-		int length = *(iter+1) - ((*iter)+1); // word length = end - start
-		// do not log spaces as words
-		if (length > 4)	{	words.push_back(line.substr(((*iter)+1), length));	}
-	}
-
-	return words;
+  return words;
 }
 
-std::map<string, int> ReadFile::UpdateMap(std::vector<string> words)
+std::map<std::string, int> UpdateMap(const std::vector<std::string> &words, std::map<std::string,int>& Counter)
 {
-	static std::map<string, int> Counter;
-	for (string word : words)
-	{
-		auto member_check = Counter.find(word);
-		// if word already a member, increment its count by 1
-		if(member_check != Counter.end())
-
-		{	Counter[word]++ ;	} 
-		else // add word to map
-		{	Counter[word] = 1;	}
-	}
-	return Counter;
+  for (auto& word : words)
+  {
+    // if word already a member, increment its count by 1
+    Counter[word]++ ;
+  }
+  return Counter;
 }
 
 // https://www.geeksforgeeks.org/sorting-a-map-by-value-in-c-stl/
-bool cmp(std::pair<string, int>& a, std::pair<string, int>& b)
+bool cmp(const std::pair<std::string, int>& a, const std::pair<std::string, int>& b)
 {
-	return a.second > b.second;
+  return a.second > b.second;
 }
 
 // Convert map into vector of pairs, sorted by value of counts
-std::vector<std::pair<string, int> > sort_by_value(std::map<string, int>& M) 
+std::vector<std::pair<std::string, int> > sort_by_value(const std::map<std::string, int>& M) 
 {
-	// Declare vector of pairs 
-	std::vector<std::pair<string, int> > A; 
+  // Declare vector of pairs 
+  std::vector<std::pair<std::string, int> > A; 
 
-	// Copy key-value pair from Map to vector of pairs 
-	for (auto& it : M) { A.push_back(it); } 
+  // Copy key-value pair from Map to vector of pairs 
+  for (auto& it : M) { A.push_back(it); } 
 
-	// Sort using comparator function 
-	sort(A.begin(), A.end(), cmp); 
+  // Sort using comparator function 
+  sort(A.begin(), A.end(), cmp); 
 
-	return A;
+  return A;
 } 
 
 int main()
 {
-	// Ask for input text to WordCount
-	std::cout << "Welcome to the WordCounter... \n\n";
-	std::cout << "Please enter the filename to analysed, e.g. MyFile.txt \n\n";
-	string filename;
-	std::cin >> filename;
+  // Ask for input text to WordCount
+  std::cout << "Welcome to the WordCounter... \n\n";
+  std::cout << "Please enter the filename to analysed, e.g. MyFile.txt \n\n";
+  std::string filename;
+  std::cin >> filename;
 
-	// Append .txt if not already contained in filename
-	size_t filename_length = filename.length();
-	if (filename_length < 4 || filename.substr(filename_length-4,filename_length) != ".txt")
-	{
-		filename += ".txt";
-	}
+  // Append .txt if not already contained in filename
+  size_t filename_length = filename.length();
+  if (filename_length < 4 || filename.substr(filename_length-4,filename_length) != ".txt")
+  {
+    filename += ".txt";
+  }
 
-	// Read in each line, Analyse and adds counts to the CounterMap
-	std::fstream fs{ filename };
-	string raw_line;
-	std::map<string, int> CounterMap;
-	while (fs)
-	{
-		std::getline(fs, raw_line);
-		ReadFile o;
-		std::vector<string> words_analysed = o.AnalyseLine(raw_line);
-		CounterMap = o.UpdateMap(words_analysed);
-	}
+  // Read in each line, Analyse and adds counts to the CounterMap
+  std::fstream fs{ filename };
+  std::string raw_line;
+  std::map<std::string, int> CounterMap;
+  while (fs)
+  {
+    std::getline(fs, raw_line);
+    std::vector<std::string> words_analysed = AnalyseLine(raw_line);
+    CounterMap = UpdateMap(words_analysed, CounterMap);
+  }
 
-	// Convert map into vector of pairs, sorted by value of counts
-	auto CounterVec = sort_by_value(CounterMap);
+  // Convert map into vector of pairs, sorted by value of counts
+  auto CounterVec = sort_by_value(CounterMap);
 
-	// Write out results to file
-	string filename_out = "Stats_WC_" + filename;
-	std::fstream fs_out{filename_out, std::ios::out};
-	fs_out << "\n \nWords                       Count\n#####                       #####\n \n";
+  // Write out results to file
+  std::string filename_out = "Stats_WC_" + filename;
+  std::fstream fs_out{filename_out, std::ios::out};
+  fs_out << "\n\nWords" << std::setw(34) << "Count\n#####" << std::setw(30) << "#####\n\n";
 
-	int is_empty = 0;
-	try{
-		if (CounterVec.size() == 0)
-		{ 
-			is_empty = 1;
-			throw filename;
-		}
-	}
-	catch (string x)
-	{
-		std::cout << "\n ***The input file " << x << " could not be analysed by WordCount.*** \
-		\nPlease check you have the correct name for the input text file, e.g. MyFile.txt.\n\n";
-	}
+  int is_empty = 0;
+  try{
+    if (CounterVec.size() == 0)
+    { 
+      is_empty = 1;
+      throw filename;
+    }
+  }
+  catch (std::string x)
+  {
+    std::cout << "\n ***The input file " << x << " could not be analysed by WordCount.*** \
+    \nPlease check you have the correct name for the input text file, e.g. MyFile.txt.\n\n";
+  }
 
-	if (is_empty == 0)
-	{
-		for (auto el : CounterVec)
-		{
-			size_t word_length = el.first.length();
-			fs_out << el.first << std::string(30-word_length, ' ') << el.second << "\n";
-		}
-		std::cout << "WordCount results are saved to " << filename_out << "\n";
-	}
+  if (is_empty == 0)
+  {
+    for (auto el : CounterVec)
+    {
+      size_t word_length = el.first.length();
+      fs_out << el.first << std::string(30-word_length, ' ') << el.second << "\n";
+    }
+    std::cout << "WordCount results are saved to " << filename_out << "\n";
+  }
 }
