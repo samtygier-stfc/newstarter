39a9df3917094d801507304d5578f4068c7e5b7b
Daniel Murphy <daniel.murphy@stfc.ac.uk>
2020-12-17 18:13:48 +0000

    Minor Ex1 changes
    UpdateMap() now outputs void as map passed by ref
    Remove outdated counter comment
    Clarify sort with ns std::
    Use camelCase
    Remove UpdateMap fn as too small
    Sort with Lambda fn

--- exercises/Daniel_Murphy/ex01_basics/src/main.cpp
+++ exercises/Daniel_Murphy/ex01_basics/src/main.cpp
@@ -8,7 +8,7 @@
 #include <iomanip>
 #include <sstream>
 
-std::vector<std::string> AnalyseLine(std::string line)
+std::vector<std::string> analyseLine(std::string line)
 {
   // Remove punctuation that accompanies a space
   std::vector<char> punctuation = {'?','.',',', '\'', '"', '(', ')', '!', ':', ';'};
@@ -35,24 +35,11 @@ std::vector<std::string> AnalyseLine(std::string line)
   return words;
 }
 
-std::map<std::string, int> UpdateMap(const std::vector<std::string> &words, std::map<std::string,int>& Counter)
-{
-  for (auto& word : words)
-  {
-    // if word already a member, increment its count by 1
-    Counter[word]++ ;
-  }
-  return Counter;
-}
 
 // https://www.geeksforgeeks.org/sorting-a-map-by-value-in-c-stl/
-bool cmp(const std::pair<std::string, int>& a, const std::pair<std::string, int>& b)
-{
-  return a.second > b.second;
-}
 
 // Convert map into vector of pairs, sorted by value of counts
-std::vector<std::pair<std::string, int> > sort_by_value(const std::map<std::string, int>& M) 
+std::vector<std::pair<std::string, int> > sortByValue(const std::map<std::string, int>& M) 
 {
   // Declare vector of pairs 
   std::vector<std::pair<std::string, int> > A; 
@@ -61,7 +48,9 @@ std::vector<std::pair<std::string, int> > sort_by_value(const std::map<std::stri
   for (auto& it : M) { A.push_back(it); } 
 
   // Sort using comparator function 
-  sort(A.begin(), A.end(), cmp); 
+  std::sort(A.begin(), A.end(), 
+    [](const std::pair<std::string, int>& a, const std::pair<std::string, int>& b)
+    { return a.second > b.second; }); 
 
   return A;
 } 
@@ -75,36 +64,40 @@ int main()
   std::cin >> filename;
 
   // Append .txt if not already contained in filename
-  size_t filename_length = filename.length();
-  if (filename_length < 4 || filename.substr(filename_length-4,filename_length) != ".txt")
+  size_t filenameLength = filename.length();
+  if (filenameLength < 4 || filename.substr(filenameLength-4,filenameLength) != ".txt")
   {
     filename += ".txt";
   }
 
   // Read in each line, Analyse and adds counts to the CounterMap
   std::fstream fs{ filename };
-  std::string raw_line;
-  std::map<std::string, int> CounterMap;
+  std::string rawLine;
+  std::map<std::string, int> counterMap;
   while (fs)
   {
-    std::getline(fs, raw_line);
-    std::vector<std::string> words_analysed = AnalyseLine(raw_line);
-    CounterMap = UpdateMap(words_analysed, CounterMap);
+    std::getline(fs, rawLine);
+    std::vector<std::string> wordsAnalysed = analyseLine(rawLine);
+    // Update counterMap
+    for (auto& word : wordsAnalysed)
+    {
+      counterMap[word]++ ;
+    }
   }
 
   // Convert map into vector of pairs, sorted by value of counts
-  auto CounterVec = sort_by_value(CounterMap);
+  auto counterVec = sortByValue(counterMap);
 
   // Write out results to file
-  std::string filename_out = "Stats_WC_" + filename;
-  std::fstream fs_out{filename_out, std::ios::out};
+  std::string filenameOut = "Stats_WC_" + filename;
+  std::fstream fs_out{filenameOut, std::ios::out};
   fs_out << "\n\nWords" << std::setw(34) << "Count\n#####" << std::setw(30) << "#####\n\n";
 
-  int is_empty = 0;
+  int isEmpty = 0;
   try{
-    if (CounterVec.size() == 0)
+    if (counterVec.size() == 0)
     { 
-      is_empty = 1;
+      isEmpty = 1;
       throw filename;
     }
   }
@@ -114,13 +107,13 @@ int main()
     \nPlease check you have the correct name for the input text file, e.g. MyFile.txt.\n\n";
   }
 
-  if (is_empty == 0)
+  if (isEmpty == 0)
   {
-    for (auto el : CounterVec)
+    for (auto el : counterVec)
     {
-      size_t word_length = el.first.length();
-      fs_out << el.first << std::string(30-word_length, ' ') << el.second << "\n";
+      size_t wordLength = el.first.length();
+      fs_out << el.first << std::string(30-wordLength, ' ') << el.second << "\n";
     }
-    std::cout << "WordCount results are saved to " << filename_out << "\n";
+    std::cout << "WordCount results are saved to " << filenameOut << "\n";
   }
 }
