c6d0ce9fb42e7abef617af7291876baf788f11ea
srikanthravipati <rsri131@gmail.com>
2021-02-05 16:27:47 +0000

    Added a few assertion tests

--- exercises/srikanth_ravipati/ex01_basics/src/Test_UniqueWords.cpp
+++ exercises/srikanth_ravipati/ex01_basics/src/Test_UniqueWords.cpp
@@ -8,6 +8,12 @@ int main(){
     Info_UniqueWords UniqueWords;
     UniqueWords.AppendElement("Test");
 
+    assert (UniqueWords.GetSize() == 1);
+    assert (UniqueWords.GetElementValue(0) == "Test");
+    assert (UniqueWords.GetElementIdx("Test") == 0);
+    assert (UniqueWords.GetElementCount("Test") == 1);
+    assert (UniqueWords.GetElementIdx("Random") == -1);
+
 
     return 0;
 }
