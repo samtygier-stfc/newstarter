fed1b30f0ae28994ada9ce3916b7b22c0e1d51ef
srikanthravipati <rsri131@gmail.com>
2021-02-05 16:29:47 +0000

    Increased counter for an element

--- exercises/srikanth_ravipati/ex01_basics/src/Test_UniqueWords.cpp
+++ exercises/srikanth_ravipati/ex01_basics/src/Test_UniqueWords.cpp
@@ -14,6 +14,7 @@ int main(){
     assert (UniqueWords.GetElementCount("Test") == 1);
     assert (UniqueWords.GetElementIdx("Random") == -1);
 
+    UniqueWords.IncrElementCount("Test");
 
     return 0;
 }
