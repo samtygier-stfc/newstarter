f737b6320809e4053c9d23db3605e392e7f22776
srikanthravipati <rsri131@gmail.com>
2021-02-08 12:18:55 +0000

    Changed to new naming convention
    Also, removed unnecessary imports

--- exercises/srikanth_ravipati/ex01_basics/src/Test_UniqueWords.cpp
+++ exercises/srikanth_ravipati/ex01_basics/src/Test_UniqueWords.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
-#include <string>
-#include <vector>
 #include <assert.h>
 #include "Info_UniqueWords.h"
 
 int main(){
-    Info_UniqueWords UniqueWords;
-    UniqueWords.AppendElement("Test");
+    infoUniqueWords uniqueWords;
+    uniqueWords.appendElement("Test");
 
-    assert (UniqueWords.GetSize() == 1);
-    assert (UniqueWords.GetElementValue(0) == "Test");
-    assert (UniqueWords.GetElementIdx("Test") == 0);
-    assert (UniqueWords.GetElementCount("Test") == 1);
-    assert (UniqueWords.GetElementIdx("Random") == -1);
+    assert (uniqueWords.getSize() == 1);
+    assert (uniqueWords.getElementValue(0) == "Test");
+    assert (uniqueWords.getElementIdx("Test") == 0);
+    assert (uniqueWords.getElementCount("Test") == 1);
+    assert (uniqueWords.getElementIdx("Random") == -1);
 
-    UniqueWords.IncrElementCount("Test");
-    assert (UniqueWords.GetElementCount("Test") == 2);
+    uniqueWords.incrElementCount("Test");
+    assert (uniqueWords.getElementCount("Test") == 2);
 
     return 0;
 }
