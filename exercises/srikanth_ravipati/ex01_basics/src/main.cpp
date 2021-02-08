989c687065f3d16ab3a3a0d2836898771120565b
srikanthravipati <rsri131@gmail.com>
2021-02-08 15:28:11 +0000

    Wrote necessary info to outputFile

--- exercises/srikanth_ravipati/ex01_basics/src/main.cpp
+++ exercises/srikanth_ravipati/ex01_basics/src/main.cpp
@@ -35,5 +35,18 @@ int main(int argc, char** argv)
 
     inputFile.close();
 
+    std::string nameOutFile = "outfile.txt";
+    std::ofstream outputFile;
+    int idx;
+
+    outputFile.open(nameOutFile, std::ios::out);
+    if (outputFile.is_open()){
+       outputFile << "Word" << "\t" << "Usage" << "\n";
+       for (int i=0; i < uniqueWords.getSize(); i++){
+           outputFile << uniqueWords.getElementValue(i) << "\t" << uniqueWords.getElementCount(i) << "\n";
+       }
+    }
+    outputFile.close();
+
     return 0;
 }
