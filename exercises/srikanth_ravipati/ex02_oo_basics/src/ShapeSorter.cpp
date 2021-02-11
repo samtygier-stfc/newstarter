3388a63b2852023c63cb2e6ffe67b26a2f59f823
srikanthravipati <rsri131@gmail.com>
2021-02-11 14:41:41 +0000

    Declared ShapeSorter class and defined associated member functions

---
+++ exercises/srikanth_ravipati/ex02_oo_basics/src/ShapeSorter.cpp
@@ -0,0 +1,23 @@
+#include "ShapeSorter.h"
+
+void ShapeSorter::areaDescent(std::vector<std::unique_ptr<Shape>>& vecShape){
+    std::cout << "\n" << "Sort and print shapes in the order of area descent " << "\n";
+    sort(vecShape.begin(), vecShape.end(), [] (auto& itrShape1, auto& itrShape2) {return itrShape1->getArea() > itrShape2->getArea();} );
+    for_each(vecShape.begin(), vecShape.end(), [] (const auto& itrShape) { itrShape->printInfo(); });
+}
+
+void ShapeSorter::perimeterDescent(std::vector<std::unique_ptr<Shape>>& vecShape){
+    std::cout << "\n" << "Sort and print shapes in the order of perimeter descent " << "\n";
+    sort(vecShape.begin(), vecShape.end(), [] (auto& itrShape1, auto& itrShape2) {return itrShape1->getPerimeter() > itrShape2->getPerimeter();} );
+    for_each(vecShape.begin(), vecShape.end(), [] (const auto& itrShape) { itrShape->printInfo(); });
+}
+
+void ShapeSorter::equal_N_Sides(const std::vector<std::unique_ptr<Shape>>& vecShape, const unsigned int& nSides){
+    std::cout << "\n" << "Print shapes that have given number of sides " << "\n";
+    for_each(vecShape.begin(), vecShape.end(), [&nSides] (const auto& itrShape) { if(itrShape->hasEqual_N_Sides(nSides)){itrShape->printInfo();}; });
+}
+
+void ShapeSorter::sameType(const std::vector<std::unique_ptr<Shape>>& vecShape, const std::string& type){
+    std::cout << "\n" << "Print shapes that matches given type " << "\n";
+    for_each(vecShape.begin(), vecShape.end(), [&type] (const auto& itrShape) { if(itrShape->isSameType(type)){itrShape->printInfo();}; });
+}
---
+++ exercises/srikanth_ravipati/ex02_oo_basics/src/ShapeSorter.h
@@ -0,0 +1,14 @@
+#include "Shape.h"
+#include <vector>
+#include <memory>
+#include <algorithm>
+
+class ShapeSorter{
+
+    public:
+    ShapeSorter(){};
+    void areaDescent(std::vector<std::unique_ptr<Shape>>&);
+    void perimeterDescent(std::vector<std::unique_ptr<Shape>>&);
+    void equal_N_Sides(const std::vector<std::unique_ptr<Shape>>&, const unsigned int&);
+    void sameType(const std::vector<std::unique_ptr<Shape>>&, const std::string&);
+};
