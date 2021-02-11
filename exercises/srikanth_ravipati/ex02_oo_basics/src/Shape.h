dfd8eb5530f0c83468f143a41f0d82d371a54be7
srikanthravipati <rsri131@gmail.com>
2021-02-11 14:25:35 +0000

    Declared Shape class
    and also defined inline member functions

---
+++ exercises/srikanth_ravipati/ex02_oo_basics/src/Shape.h
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "cmath"
+
+class Shape{
+
+    protected:
+        std::string type;
+        unsigned int nSides;
+        double perimeter;
+        double area;
+
+    public:
+
+        Shape(const std::string&, const unsigned int&);
+
+        virtual void calculatePerimeter(){}
+        virtual void calculateArea(){}
+        void calculateInfo();
+
+        std::string getType() const {return type;}
+        unsigned int get_N_Sides() const {return nSides;}
+        double getPerimeter() const { return perimeter; }
+        double getArea() const { return area; }
+
+        bool isSameType(const std::string& inputType) const { return type == inputType;}
+        bool hasEqual_N_Sides(const int& input_N_Sides) const { return nSides == input_N_Sides;}
+
+        void printInfo() const;
+};
