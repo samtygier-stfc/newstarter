ae1d74693e87fab0c9d75e5890222c8daea5c4a7
srikanthravipati <rsri131@gmail.com>
2021-02-11 14:29:53 +0000

    Declared and defined Rectangle derived class

---
+++ exercises/srikanth_ravipati/ex02_oo_basics/src/Rectangle.cpp
@@ -0,0 +1,6 @@
+#include "Shape.h"
+
+Rectangle::Rectangle(const std::string& inputType, const unsigned int& input_N_Sides, const double& inputSide1, const double& inputSide2)
+               : Shape::Shape(inputType, input_N_Sides), side1 { inputSide1 }, side2 { inputSide2 }
+           {
+           }
--- exercises/srikanth_ravipati/ex02_oo_basics/src/Shape.h
+++ exercises/srikanth_ravipati/ex02_oo_basics/src/Shape.h
@@ -27,3 +27,14 @@ class Shape{
 
         void printInfo() const;
 };
+
+class Rectangle : public Shape{
+
+    private:
+        double side1, side2;
+
+    public:
+        Rectangle(const std::string&, const unsigned int&, const double&, const double&);
+        void calculatePerimeter() override { perimeter = 2 * (side1 + side2); }
+        void calculateArea() override { area = side1 * side2; }
+};
