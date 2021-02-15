0c7b680654d49a204c1b5b336dee2f1bf3f4ba8d
srikanthravipati <rsri131@gmail.com>
2021-02-15 16:54:47 +0000

    Broke long lines into multiple lines

--- exercises/srikanth_ravipati/ex01_basics/src/main.cpp
+++ exercises/srikanth_ravipati/ex01_basics/src/main.cpp
@@ -6,7 +6,8 @@
 int main(int argc, char** argv){
 
     if (argc < 2){
-        throw std::runtime_error("Atleast one argument (filename) should be passed. USAGE: ./a.out inputFileName");
+        throw std::runtime_error("Atleast one argument (filename) should be "
+                                 "passed. USAGE: ./a.out inputFileName");
         return 1;
     } else {
         std::cout << "File to be processed: "  << argv[1] << "\n";
@@ -14,7 +15,8 @@ int main(int argc, char** argv){
 
     auto nameInFile = argv[1];
 
-    std::vector<char> delimiters = {'\'', '\"', '.', ',', '?', '!', '(', ')', ':', ' ', '-', '}'};
+    std::vector<char> delimiters = { '\'', '\"', '.', ',', '?', '!',
+                                     '(', ')', ':', ' ', '-', '}' };
     std::map<std::string, int> mapWordCounter;
 
     std::ifstream inputFile;
@@ -33,12 +35,14 @@ int main(int argc, char** argv){
         vecWordCounter.push_back(itr);
     }
 
-    sort(vecWordCounter.begin(), vecWordCounter.end(), [](auto& a, auto& b){ return a.second > b.second;});
+    sort(vecWordCounter.begin(), vecWordCounter.end(),
+         [](auto& a, auto& b){ return a.second > b.second;});
 
     std::string nameOutFile = "outfile.txt";
     std::ofstream outputFile;
     outputFile.open(nameOutFile, std::ios::out);
-    for_each(vecWordCounter.cbegin(), vecWordCounter.cend(), [&outputFile](auto& itr) {outputFile << itr.first << "\t" << itr.second << "\n";});
+    for_each(vecWordCounter.cbegin(), vecWordCounter.cend(),
+             [&outputFile](auto& itr) {outputFile << itr.first << "\t" << itr.second << "\n";});
     outputFile.close();
 
 }
