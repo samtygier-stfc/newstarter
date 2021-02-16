dcaf88022f4da91ed507274175d764e88e7d917f
srikanthravipati <rsri131@gmail.com>
2021-02-16 14:08:05 +0000

    Done format changes

--- exercises/srikanth_ravipati/ex02_oo_basics/src/Circle.cpp
+++ exercises/srikanth_ravipati/ex02_oo_basics/src/Circle.cpp
@@ -1,6 +1,7 @@
 #include "Circle.h"
 
-Circle::Circle(const std::string& inputType, const unsigned int& input_N_Sides, const double& inputRadius)
-            : Shape::Shape(inputType, input_N_Sides), radius { inputRadius }
-        {
-        }
+Circle::Circle(const std::string &inputType, const unsigned int &input_N_Sides, 
+               const double &inputRadius)
+    : Shape::Shape(inputType, input_N_Sides), radius { inputRadius }
+    {
+    }
