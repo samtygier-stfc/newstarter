119f88d8864dbfdea60dfd873ee0b2194a6da0c2
srikanthravipati <rsri131@gmail.com>
2021-02-16 12:11:01 +0000

    Replaced for_each with for-loop

--- exercises/srikanth_ravipati/ex02_oo_basics/src/ShapeSorter.cpp
+++ exercises/srikanth_ravipati/ex02_oo_basics/src/ShapeSorter.cpp
@@ -4,28 +4,32 @@ void ShapeSorter::areaDescent(std::vector<std::unique_ptr<Shape>> &vecShape) {
   std::cout << "\n" << "Sort and print shapes in the order of area descent " << "\n";
   std::sort(vecShape.begin(), vecShape.end(), [] (auto &itrShape1, auto &itrShape2)
     { return itrShape1->getArea() > itrShape2->getArea(); });
-  for_each(vecShape.begin(), vecShape.end(), 
-    [] (const auto &itrShape) { itrShape->printInfo(); });
+  for (auto &vecShapeElement : vecShape) {
+    vecShapeElement->printInfo();
+  }
 }
 
 void ShapeSorter::perimeterDescent(std::vector<std::unique_ptr<Shape>> &vecShape) {
   std::cout << "\n" << "Sort and print shapes in the order of perimeter descent " << "\n";
   std::sort(vecShape.begin(), vecShape.end(), [] (auto &itrShape1, auto &itrShape2)
     { return itrShape1->getPerimeter() > itrShape2->getPerimeter(); } );
-  for_each(vecShape.begin(), vecShape.end(), 
-    [] (const auto &itrShape) { itrShape->printInfo(); });
+  for (auto &vecShapeElement : vecShape) {
+    vecShapeElement->printInfo();
+  }
 }
 
 void ShapeSorter::equal_N_Sides(const std::vector<std::unique_ptr<Shape>> &vecShape, 
   const unsigned int &nSides) {
   std::cout << "\n" << "Print shapes that have given number of sides " << "\n";
-  for_each(vecShape.begin(), vecShape.end(), [&nSides] (const auto &itrShape) 
-      { if(itrShape->hasEqual_N_Sides(nSides)) { itrShape->printInfo(); }; });
+  for (auto &vecShapeElement : vecShape) {
+    if (vecShapeElement->hasEqual_N_Sides(nSides)) { vecShapeElement->printInfo(); };
+  }
 }
 
 void ShapeSorter::sameType(const std::vector<std::unique_ptr<Shape>> &vecShape, 
   const std::string &type) {
   std::cout << "\n" << "Print shapes that matches given type " << "\n";
-  for_each(vecShape.begin(), vecShape.end(), [&type] (const auto &itrShape) 
-    { if(itrShape->isSameType(type)) { itrShape->printInfo(); }; });
+  for (auto &vecShapeElement : vecShape) {
+    if (vecShapeElement->isSameType(type)) { vecShapeElement->printInfo(); };
+  }
 }
