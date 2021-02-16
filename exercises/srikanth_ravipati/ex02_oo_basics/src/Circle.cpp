43603895ec715efef84e7944a28108c2ccaf649c
srikanthravipati <rsri131@gmail.com>
2021-02-16 13:43:03 +0000

    Included correct header file

--- exercises/srikanth_ravipati/ex02_oo_basics/src/Circle.cpp
+++ exercises/srikanth_ravipati/ex02_oo_basics/src/Circle.cpp
@@ -1,4 +1,4 @@
-#include "Shape.h"
+#include "Circle.h"
 
 Circle::Circle(const std::string& inputType, const unsigned int& input_N_Sides, const double& inputRadius)
             : Shape::Shape(inputType, input_N_Sides), radius { inputRadius }
