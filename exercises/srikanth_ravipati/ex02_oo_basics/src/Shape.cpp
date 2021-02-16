c542d0b069f568325181593c2f0b0eeaec79711b
srikanthravipati <rsri131@gmail.com>
2021-02-16 11:46:54 +0000

    Made format changes

--- exercises/srikanth_ravipati/ex02_oo_basics/src/Shape.cpp
+++ exercises/srikanth_ravipati/ex02_oo_basics/src/Shape.cpp
@@ -1,16 +1,18 @@
 #include "Shape.h"
 
-Shape::Shape(const std::string& inputType, const unsigned int& input_N_Sides)
-    : type { inputType }, nSides {input_N_Sides}
+Shape::Shape(const std::string &inputType, const unsigned int &input_N_Sides)
+  : type { inputType }, nSides { input_N_Sides }
 {
 }
 
-void Shape::calculateInfo(){
-         calculatePerimeter();
-         calculateArea();
-       }
+void Shape::calculateInfo() {
+  calculatePerimeter();
+  calculateArea();
+}
 
 void Shape::printInfo() const {
-           std::cout << "Type: " << type << "\t" << "nSides: " << nSides <<
-                        "\t" << "Perimeter: " << perimeter << "\t" << "Area: " << area << "\n";
-       }
+  std::cout << "Type: " << type << "\t"
+            << "nSides: " << nSides << "\t" 
+            << "Perimeter: " << perimeter << "\t" 
+            << "Area: " << area << "\n";
+}
