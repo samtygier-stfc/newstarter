6be84642061cc1e24e35b23f2da1f9d36627c349
srikanthravipati <rsri131@gmail.com>
2021-02-16 14:08:00 +0000

    Done format changes

--- exercises/srikanth_ravipati/ex02_oo_basics/src/Rectangle.cpp
+++ exercises/srikanth_ravipati/ex02_oo_basics/src/Rectangle.cpp
@@ -1,11 +1,13 @@
 #include "Rectangle.h"
 
-Rectangle::Rectangle(const std::string& inputType, const unsigned int& input_N_Sides, const double& inputSide1, const double& inputSide2)
-               : Shape::Shape(inputType, input_N_Sides), side1 { inputSide1 }, side2 { inputSide2 }
-           {
-           }
+Rectangle::Rectangle(const std::string &inputType, const unsigned int &input_N_Sides,
+                     const double &inputSide1, const double &inputSide2)
+    : Shape::Shape(inputType, input_N_Sides), side1 { inputSide1 }, side2 { inputSide2 }
+    {
+    }
 
-Rectangle::Rectangle(const std::string& inputType, const unsigned int& input_N_Sides, const double& inputSide)
-               : Shape::Shape(inputType, input_N_Sides), side1 { inputSide }, side2 { inputSide }
-           {
-           }
+Rectangle::Rectangle(const std::string &inputType, const unsigned int &input_N_Sides, 
+                     const double &inputSide)
+    : Shape::Shape(inputType, input_N_Sides), side1 { inputSide }, side2 { inputSide }
+    {
+    }
