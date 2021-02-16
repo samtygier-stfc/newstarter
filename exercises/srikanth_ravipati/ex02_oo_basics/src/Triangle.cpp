e101822d8b07473a71b1d5ab4654ed6d21e9238d
srikanthravipati <rsri131@gmail.com>
2021-02-16 13:43:10 +0000

    Included correct header file

--- exercises/srikanth_ravipati/ex02_oo_basics/src/Triangle.cpp
+++ exercises/srikanth_ravipati/ex02_oo_basics/src/Triangle.cpp
@@ -1,4 +1,4 @@
-#include "Shape.h"
+#include "Triangle.h"
 
 Triangle::Triangle(const std::string& inputType, const unsigned int& input_N_Sides, const double& inputBase, const double& inputHeight)
               : Shape::Shape(inputType, input_N_Sides), base { inputBase }, height { inputHeight }
