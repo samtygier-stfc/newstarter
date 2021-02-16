ad43b441718dae286710de9f32d5894ae12f208e
srikanthravipati <rsri131@gmail.com>
2021-02-16 13:42:50 +0000

    Included correct header file

--- exercises/srikanth_ravipati/ex02_oo_basics/src/Rectangle.cpp
+++ exercises/srikanth_ravipati/ex02_oo_basics/src/Rectangle.cpp
@@ -1,4 +1,4 @@
-#include "Shape.h"
+#include "Rectangle.h"
 
 Rectangle::Rectangle(const std::string& inputType, const unsigned int& input_N_Sides, const double& inputSide1, const double& inputSide2)
                : Shape::Shape(inputType, input_N_Sides), side1 { inputSide1 }, side2 { inputSide2 }
