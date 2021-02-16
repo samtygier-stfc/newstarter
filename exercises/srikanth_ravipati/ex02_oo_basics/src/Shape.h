1e0b6c38e16d1b2559a7f31b885b4befec2f6aff
srikanthravipati <rsri131@gmail.com>
2021-02-16 12:17:23 +0000

    Broke long line into multiple lines

--- exercises/srikanth_ravipati/ex02_oo_basics/src/Shape.h
+++ exercises/srikanth_ravipati/ex02_oo_basics/src/Shape.h
@@ -59,7 +59,8 @@ private:
   double base, height;
 
 public:
-  Triangle(const std::string &, const unsigned int &, const double &, const double &);
+  Triangle(const std::string &, const unsigned int &, const double &, 
+           const double &);
   void calculatePerimeter() override 
     { perimeter = base + 2 * sqrt(height * height + (base * base) / 4); }
   void calculateArea() override { area = 0.5 * base * height; }
