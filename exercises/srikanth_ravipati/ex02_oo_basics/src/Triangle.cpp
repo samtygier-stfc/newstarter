1af39aa73803d731464348fb8ce2f5ea9bc85708
srikanthravipati <rsri131@gmail.com>
2021-02-16 14:08:23 +0000

    Done format changes

--- exercises/srikanth_ravipati/ex02_oo_basics/src/Triangle.cpp
+++ exercises/srikanth_ravipati/ex02_oo_basics/src/Triangle.cpp
@@ -1,6 +1,7 @@
 #include "Triangle.h"
 
-Triangle::Triangle(const std::string& inputType, const unsigned int& input_N_Sides, const double& inputBase, const double& inputHeight)
-              : Shape::Shape(inputType, input_N_Sides), base { inputBase }, height { inputHeight }
-          {
-          }
+Triangle::Triangle(const std::string &inputType, const unsigned int &input_N_Sides, 
+                   const double &inputBase, const double &inputHeight)
+    : Shape::Shape(inputType, input_N_Sides), base { inputBase }, height { inputHeight }
+    {
+    }
