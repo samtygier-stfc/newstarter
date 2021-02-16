cea45f05dfee6d3c93dc0bd2227a1507be8c5ba2
srikanthravipati <rsri131@gmail.com>
2021-02-16 11:46:45 +0000

    Made format changes

--- exercises/srikanth_ravipati/ex02_oo_basics/src/Shape.h
+++ exercises/srikanth_ravipati/ex02_oo_basics/src/Shape.h
@@ -1,63 +1,66 @@
 #include <iostream>
 #include <cmath>
 
-class Shape{
+class Shape {
 
-    protected:
-        std::string type;
-        unsigned int nSides;
-        double perimeter;
-        double area;
+protected:
+  std::string type;
+  unsigned int nSides;
+  double perimeter;
+  double area;
 
-    public:
+public:
+  Shape(const std::string &, const unsigned int &);
 
-        Shape(const std::string&, const unsigned int&);
+  virtual void calculatePerimeter(){}
+  virtual void calculateArea(){}
+  void calculateInfo();
 
-        virtual void calculatePerimeter(){}
-        virtual void calculateArea(){}
-        void calculateInfo();
+  std::string getType() const { return type; }
+  unsigned int get_N_Sides() const { return nSides; }
+  double getPerimeter() const { return perimeter; }
+  double getArea() const { return area; }
 
-        std::string getType() const {return type;}
-        unsigned int get_N_Sides() const {return nSides;}
-        double getPerimeter() const { return perimeter; }
-        double getArea() const { return area; }
+  bool isSameType(const std::string &inputType) const 
+    { return type == inputType; }
+  bool hasEqual_N_Sides(const unsigned int &input_N_Sides) const 
+    { return nSides == input_N_Sides; }
 
-        bool isSameType(const std::string& inputType) const { return type == inputType;}
-        bool hasEqual_N_Sides(const unsigned int& input_N_Sides) const { return nSides == input_N_Sides;}
-
-        void printInfo() const;
+  void printInfo() const;
 };
 
-class Rectangle : public Shape{
+class Rectangle : public Shape {
 
-    private:
-        double side1, side2;
+private:
+  double side1, side2;
 
-    public:
-        Rectangle(const std::string&, const unsigned int&, const double&, const double&);
-        Rectangle(const std::string&, const unsigned int&, const double&);
-        void calculatePerimeter() override { perimeter = 2 * (side1 + side2); }
-        void calculateArea() override { area = side1 * side2; }
+public:
+  Rectangle(const std::string &, const unsigned int &, const double &, 
+    const double &);
+  Rectangle(const std::string &, const unsigned int &, const double &);
+  void calculatePerimeter() override { perimeter = 2 * (side1 + side2); }
+  void calculateArea() override { area = side1 * side2; }
 };
 
-class Circle : public Shape{
+class Circle : public Shape {
 
-    private:
-        double radius;
+private:
+  double radius;
 
-    public:
-        Circle(const std::string&, const unsigned int&, const double&);
-        void calculatePerimeter() override { perimeter = 2.0 * M_PI * radius; }
-        void calculateArea() override { area = M_PI * radius  * radius; }
+public:
+  Circle(const std::string &, const unsigned int &, const double &);
+  void calculatePerimeter() override { perimeter = 2.0 * M_PI * radius; }
+  void calculateArea() override { area = M_PI * radius  * radius; }
 };
 
-class Triangle : public Shape{
+class Triangle : public Shape {
 
-    private:
-        double base, height;
+private:
+  double base, height;
 
-    public:
-        Triangle(const std::string& inputType, const unsigned int&, const double&, const double&);
-        void calculatePerimeter() override { perimeter = base + 2 * sqrt(height * height + (base * base) / 4); }
-        void calculateArea() override { area = 0.5 * base * height; }
+public:
+  Triangle(const std::string &, const unsigned int &, const double &, const double &);
+  void calculatePerimeter() override 
+    { perimeter = base + 2 * sqrt(height * height + (base * base) / 4); }
+  void calculateArea() override { area = 0.5 * base * height; }
 };
